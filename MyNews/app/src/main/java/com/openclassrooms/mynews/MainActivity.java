c731b718a3730ca48bc82d9c9fb699726ff8d134
leoberenger <leoberenger@gmail.com>
2018-02-26 10:14:40 +0100

    Linked toolbar to MainActivity

--- MyNews/app/src/main/java/com/openclassrooms/mynews/MainActivity.java
+++ MyNews/app/src/main/java/com/openclassrooms/mynews/MainActivity.java
@@ -2,6 +2,8 @@ package com.openclassrooms.mynews;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        this.configureToolbar();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_activity_main, menu);
+        return true;
+    }
+
+    private void configureToolbar(){
+        android.support.v7.widget.Toolbar toolbar
+                = (android.support.v7.widget.Toolbar) findViewById(R.id.activity_main_toolbar);
+        setSupportActionBar(toolbar);
     }
 }
