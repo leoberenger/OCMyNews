4a9ee2a2ea78db201198831a6df30d8d07fa7d98
leoberenger <leoberenger@gmail.com>
2018-02-26 10:21:16 +0100

    Added Clic on menu items -> toast

--- MyNews/app/src/main/java/com/openclassrooms/mynews/MainActivity.java
+++ MyNews/app/src/main/java/com/openclassrooms/mynews/MainActivity.java
@@ -3,6 +3,8 @@ package com.openclassrooms.mynews;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
@@ -18,6 +20,23 @@ public class MainActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu){
         getMenuInflater().inflate(R.menu.menu_activity_main, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        switch (item.getItemId()){
+            case R.id.menu_navMenu:
+                Toast.makeText(this, "Menu de Navigation indisponible", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.menu_search:
+                Toast.makeText(this, "Recherche indisponible", Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.menu_more:
+                Toast.makeText(this, "En savoir plus indisponible", Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     private void configureToolbar(){
