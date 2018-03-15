4292e4253dd1de7282fec4438627d172aa811206
leoberenger <leoberenger@gmail.com>
2018-03-15 11:09:29 +0100

    Cleaned DetailActivity and DetailFragment

--- MyNews/app/src/main/java/com/openclassrooms/mynews/Controllers/Activities/DetailActivity.java
+++ MyNews/app/src/main/java/com/openclassrooms/mynews/Controllers/Activities/DetailActivity.java
@@ -4,8 +4,9 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.openclassrooms.netapp.Controllers.Fragments.DetailFragment;
-import com.openclassrooms.netapp.R;
+import com.openclassrooms.mynews.Controllers.Fragments.DetailFragment;
+import com.openclassrooms.mynews.R;
+
 
 public class DetailActivity extends AppCompatActivity {
 
--- MyNews/app/src/main/java/com/openclassrooms/mynews/Controllers/Fragments/DetailFragment.java
+++ MyNews/app/src/main/java/com/openclassrooms/mynews/Controllers/Fragments/DetailFragment.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.webkit.WebView;
 
-import com.openclassrooms.netapp.R;
+import com.openclassrooms.mynews.R;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
