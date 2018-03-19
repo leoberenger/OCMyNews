7a40f665f33faa3cbd78d050574e343a913c6a52
leoberenger <leoberenger@gmail.com>
2018-03-19 10:51:54 +0100

    Created SearchArticleFragment from TopicFragment

---
+++ MyNews/app/src/main/java/com/openclassrooms/mynews/Controllers/Fragments/SearchArticleFragment.java
@@ -0,0 +1,166 @@
+package com.openclassrooms.mynews.Controllers.Fragments;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.bumptech.glide.Glide;
+import com.openclassrooms.mynews.Controllers.Activities.DetailActivity;
+import com.openclassrooms.mynews.Models.NYTimesAPI;
+import com.openclassrooms.mynews.Models.Response;
+import com.openclassrooms.mynews.R;
+import com.openclassrooms.mynews.Utils.ItemClickSupport;
+import com.openclassrooms.mynews.Utils.NYTStreams;
+import com.openclassrooms.mynews.Views.SearchArticleAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import io.reactivex.disposables.Disposable;
+import io.reactivex.observers.DisposableObserver;
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class SearchArticleFragment extends Fragment {
+
+
+    // FOR DESIGN
+    @BindView(R.id.fragment_main_recycler_view)
+    RecyclerView mRecyclerView;
+    @BindView(R.id.fragment_main_swipe_container)
+    SwipeRefreshLayout mSwipeRefreshLayout;
+
+    //FOR DATA
+    private Disposable mDisposable;
+    private List<Response.Doc> articles;
+    private SearchArticleAdapter mAdapter;
+    private io.reactivex.Observable<NYTimesAPI> stream;
+
+    //SEARCH ARTICLE QUERIES
+    String mQuery = "france";
+    String mNewsDesk; //"news_desk:(%22Travel%22)";
+    int mBeginDate = 20170910;
+    int mEndDate = 20171001;
+
+    String EXTRA_ARTICLE_URL = "EXTRA_ARTICLE_URL";
+
+    public SearchArticleFragment() { }
+
+    public static SearchArticleFragment newInstance(String newsDesk){
+        SearchArticleFragment topicFragment = new SearchArticleFragment();
+
+        Bundle args = new Bundle();
+        args.putString("newsDesk", newsDesk);
+        topicFragment.setArguments(args);
+
+        return topicFragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+        ButterKnife.bind(this, view);
+
+        Bundle args = getArguments();
+        mNewsDesk = "news_desk:(%22" + args.getString("newsDesk", "") + "%22)";
+
+        stream = NYTStreams.streamFetchSearchArticles(mQuery, mNewsDesk, mBeginDate, mEndDate);
+        this.executeHttpRequestWithRetrofit();
+        this.configureSwipeRefreshLayout();
+        this.configureRecyclerView();
+        this.configureOnClickRecyclerView();
+        return view;
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        this.disposeWhenDestroy();
+    }
+
+    // -----------------
+    // CONFIGURATION
+    // -----------------
+
+    private void configureSwipeRefreshLayout(){
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                executeHttpRequestWithRetrofit();
+            }
+        });
+    }
+
+    private void configureRecyclerView(){
+        this.articles = new ArrayList<>();
+        this.mAdapter = new SearchArticleAdapter(this.articles, Glide.with(this));
+        this.mRecyclerView.setAdapter(this.mAdapter);
+        this.mRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+    }
+
+    private void configureOnClickRecyclerView(){
+        ItemClickSupport.addTo(mRecyclerView, R.layout.fragment_main_item)
+                .setOnItemClickListener(new ItemClickSupport.OnItemClickListener() {
+                    @Override
+                    public void onItemClicked(RecyclerView recyclerView, int position, View v) {
+                        String articleUrl = mAdapter.getResponse(position).getWebUrl();
+                        Intent intent = new Intent(getActivity(), DetailActivity.class);
+                        intent.putExtra(EXTRA_ARTICLE_URL, articleUrl);
+                        startActivity(intent);
+                    }
+                });
+    }
+
+    // -----------------
+    // HTTP REQUEST (RxJava)
+    // -----------------
+
+    private void executeHttpRequestWithRetrofit(){
+        this.mDisposable = stream
+                .subscribeWith(new DisposableObserver<NYTimesAPI>(){
+                    @Override
+                    public void onNext(NYTimesAPI articles) {
+                        Log.e("TAG", "On Next");
+                        updateUI(articles);
+                    }
+
+                    @Override
+                    public void onError(Throwable e) {
+                        Log.e("TAG", "On Error"+Log.getStackTraceString(e));
+                    }
+
+                    @Override
+                    public void onComplete() {
+                        Log.e("TAG", "On Complete");
+                    }
+                });
+    }
+
+    private void disposeWhenDestroy(){
+        if(this.mDisposable != null && !this.mDisposable.isDisposed())
+            this.mDisposable.dispose();
+    }
+
+    // -----------------
+    // UPDATE UI
+    // -----------------
+
+    private void updateUI(NYTimesAPI responses){
+        mSwipeRefreshLayout.setRefreshing(false);
+        articles.clear();
+        articles.addAll(responses.getResponse().getDocs());
+        mAdapter.notifyDataSetChanged();
+    }
+}
+
