b601d16093c4da2604dd23e22673fb12131cdf97
leoberenger <leoberenger@gmail.com>
2018-03-20 22:46:51 +0100

    Added DatePickers on SearchFragment

--- MyNews/app/src/main/java/com/openclassrooms/mynews/Controllers/Fragments/SearchFragment.java
+++ MyNews/app/src/main/java/com/openclassrooms/mynews/Controllers/Fragments/SearchFragment.java
@@ -1,17 +1,28 @@
 package com.openclassrooms.mynews.Controllers.Fragments;
 
 
+import android.app.DatePickerDialog;
+import android.text.format.DateFormat;
+import android.widget.DatePicker;
+import java.util.Calendar;
+import java.util.Date;
+import android.app.Dialog;
 import android.os.Bundle;
+import android.support.v4.app.DialogFragment;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
 
 import com.openclassrooms.mynews.Controllers.Fragments.ArticleViews.DisplaySearchFragment;
 import com.openclassrooms.mynews.R;
+
+import java.util.Calendar;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -19,14 +30,20 @@ import butterknife.ButterKnife;
 /**
  * A simple {@link Fragment} subclass.
  */
-public class SearchFragment extends Fragment {
+public class SearchFragment extends Fragment{
 
     private DisplaySearchFragment mDisplaySearchFragment;
+    private DatePickerDialog beginDatePickerDialog;
+    private DatePickerDialog endDatePickerDialog;
 
     @BindView(R.id.activity_search_query_input)
     EditText queryInput;
+    @BindView(R.id.search_begin_date)
+    EditText beginDatePicker;
+    @BindView(R.id.search_end_date)
+    EditText endDatePicker;
     @BindView(R.id.activity_search_button)
-    Button button;
+    Button searchBtn;
 
     private String mQuery = "france";
     private String mNewsDesk = "news_desk:(%22Travel%22)";
@@ -35,7 +52,6 @@ public class SearchFragment extends Fragment {
 
     public SearchFragment() {}
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -43,7 +59,59 @@ public class SearchFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_search, container, false);
         ButterKnife.bind(this, view);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        // perform click event on edit text
+        beginDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // calender class's instance and get current date , month and year from calender
+                final Calendar c = Calendar.getInstance();
+                int mYear = c.get(Calendar.YEAR); // current year
+                int mMonth = c.get(Calendar.MONTH); // current month
+                int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+
+                // date picker dialog
+                beginDatePickerDialog = new DatePickerDialog(getActivity(), R.style.DialogTheme,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // set day of month , month and year value in the edit text
+                                String dateString = dayOfMonth + "/" + (monthOfYear + 1) + "/" + year;
+                                beginDatePicker.setText(dateString);
+                            }
+                        }, mYear, mMonth, mDay);
+                beginDatePickerDialog.show();
+            }
+        });
+
+        // perform click event on edit text
+        endDatePicker.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // calender class's instance and get current date , month and year from calender
+                final Calendar c = Calendar.getInstance();
+                int mYear = c.get(Calendar.YEAR); // current year
+                int mMonth = c.get(Calendar.MONTH); // current month
+                int mDay = c.get(Calendar.DAY_OF_MONTH); // current day
+
+                // date picker dialog
+                endDatePickerDialog = new DatePickerDialog(getActivity(), R.style.DialogTheme,
+                        new DatePickerDialog.OnDateSetListener() {
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // set day of month , month and year value in the edit text
+                                String dateString = dayOfMonth + "/" + (monthOfYear + 1) + "/" + year;
+                                endDatePicker.setText(dateString);
+
+                            }
+                        }, mYear, mMonth, mDay);
+                endDatePickerDialog.show();
+            }
+        });
+
+        searchBtn.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 //mQuery = queryInput.getText().toString();
                 //deskCheckboxes(view);
@@ -53,7 +121,6 @@ public class SearchFragment extends Fragment {
 
         return view;
     }
-
 
     private void configureAndShowDisplaySearchFragment() {
 
@@ -128,7 +195,5 @@ public class SearchFragment extends Fragment {
             //SHOW MESSAGE TO SELECT AT LEAST 1
         }
     }
-
-
 
 }
