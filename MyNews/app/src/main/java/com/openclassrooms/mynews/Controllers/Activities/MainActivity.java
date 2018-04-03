72699fc13d6059aef99139858d1e4799bc806f04
leoberenger <leoberenger@gmail.com>
2018-04-03 20:27:08 +0200

    Added AlertDialog to Display More Menu

--- MyNews/app/src/main/java/com/openclassrooms/mynews/Controllers/Activities/MainActivity.java
+++ MyNews/app/src/main/java/com/openclassrooms/mynews/Controllers/Activities/MainActivity.java
@@ -1,6 +1,8 @@
 package com.openclassrooms.mynews.Controllers.Activities;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.TabLayout;
@@ -8,6 +10,7 @@ import android.support.v4.view.GravityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -74,8 +77,16 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 startActivity(intent);
                 return true;
             case R.id.menu_more:
-                Intent intent2 = new Intent(this, NotificationActivity.class);
-                startActivity(intent2);
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setItems(R.array.alertDialog, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int which) {
+                                if(which == 0){
+                                    Intent intent2 = new Intent(getApplicationContext(), NotificationActivity.class);
+                                    startActivity(intent2);
+                                }
+                            }
+                        });
+                builder.create().show();
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
