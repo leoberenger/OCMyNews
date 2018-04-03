8f727e0d97c6099db60fca2feecb2aa947ee24e4
leoberenger <leoberenger@gmail.com>
2018-04-03 18:53:56 +0200

    Added AlertDialog if no Article to show

--- MyNews/app/src/main/java/com/openclassrooms/mynews/Controllers/Fragments/BaseFragment.java
+++ MyNews/app/src/main/java/com/openclassrooms/mynews/Controllers/Fragments/BaseFragment.java
@@ -9,6 +9,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.openclassrooms.mynews.Controllers.Activities.DetailActivity;
--- MyNews/app/src/main/java/com/openclassrooms/mynews/Controllers/Fragments/SearchFragment.java
+++ MyNews/app/src/main/java/com/openclassrooms/mynews/Controllers/Fragments/SearchFragment.java
@@ -1,15 +1,22 @@
 package com.openclassrooms.mynews.Controllers.Fragments;
 
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.View;
+import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
 import com.openclassrooms.mynews.Controllers.Activities.DetailActivity;
+import com.openclassrooms.mynews.Controllers.Activities.DisplaySearchActivity;
+import com.openclassrooms.mynews.Controllers.Activities.SearchActivity;
 import com.openclassrooms.mynews.Models.NYTimesAPI;
 import com.openclassrooms.mynews.Models.Response;
 import com.openclassrooms.mynews.R;
@@ -120,6 +127,23 @@ public class SearchFragment extends BaseFragment {
         articles.clear();
         articles.addAll(responses.getResponse().getDocs());
         mAdapter.notifyDataSetChanged();
+
+        //no article to show -> alertDialog
+        if(responses.getResponse().getDocs().isEmpty()) {
+            AlertDialog.Builder builder;
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+                builder = new AlertDialog.Builder(getContext(), android.R.style.Theme_Material_Dialog_Alert);
+            } else {
+                builder = new AlertDialog.Builder(getContext());
+            }
+            builder.setMessage("No Articles for this search")
+                    .setPositiveButton("New Search", new DialogInterface.OnClickListener() {
+                        public void onClick(DialogInterface dialog, int id) {
+                            Intent intent = new Intent(getActivity(), SearchActivity.class);
+                            startActivity(intent);
+                        }})
+                    .show();
+        }
     }
 }
 
